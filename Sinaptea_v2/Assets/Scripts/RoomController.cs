a7a351dfd7f9fa96122f604ead7f4f18e0385c69
Jorge Arturo Barrios Laureani <zs22017016@estudiantes.uv.mx>
2025-04-04 22:37:59 -0600

    Merge manual de ambas ramas

---
+++ Sinaptea_v2/Assets/Scripts/RoomController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RoomController : MonoBehaviour
+{
+    public Button room;
+    public GameObject blackout;
+    public Button sala;
+    public Button emociones;
+    public Button dormitorio;
+    public Button armario;
+    public Button tienda;
+    public Button comedor;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        room = GameObject.Find("Canvas/BotonHabitacion").GetComponent<Button>();
+        room.onClick.AddListener(DeployModal);
+    }
+
+    void DeployModal()
+    {
+        blackout.SetActive(!blackout.activeSelf);
+        blackout = GameObject.Find("Canvas/BotonHabitacion/Blackout");
+        sala = GameObject.Find("Canvas/BotonHabitacion/Blackout/ModalHabitaciones/BotonIrSala").GetComponent<Button>();
+        sala.onClick.AddListener(() => Navigate("SampleScene"));
+        emociones = GameObject.Find("Canvas/BotonHabitacion/Blackout/ModalHabitaciones/BotonIrEmociones").GetComponent<Button>();
+        emociones.onClick.AddListener(() => Navigate("EmocionesScene"));
+        dormitorio = GameObject.Find("Canvas/BotonHabitacion/Blackout/ModalHabitaciones/BotonIrDormitorio").GetComponent<Button>();
+        dormitorio.onClick.AddListener(() => Navigate("DormitorioScene"));
+        // To Do armario, tienda, comedor
+    }
+
+    void Navigate(string roomName)
+    {
+        SceneManager.LoadScene(roomName);
+    }
+}
