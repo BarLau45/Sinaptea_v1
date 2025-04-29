d53de98a10a2b8701ca6e2d5c759f635afa5bb94
Jorge Arturo Barrios Laureani <zs22017016@estudiantes.uv.mx>
2025-04-28 22:15:15 -0600

    Añadida conexión con Firebase

---
+++ Sinaptea_v2/Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Firebase.Analytics;
+using Firebase;
+
+public class FirebaseInit : MonoBehaviour
+{
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => 
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+}
